fc44fd7b992d90d0d4419d4886105a5277eee393
snail24365 <snail24365@gmail.com>
2021-12-10 12:38:32 +0900

    war로 배포 설정 변경

--- src/main/java/com/simple/blog/BlogApplication.java
+++ src/main/java/com/simple/blog/BlogApplication.java
@@ -8,12 +8,13 @@ import com.simple.blog.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import javax.validation.ConstraintViolationException;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
