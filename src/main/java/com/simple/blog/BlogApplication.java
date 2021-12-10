2f3ef420ea2d4374f9c313bc623ad6790387ef37
snail24365 <snail24365@gmail.com>
2021-12-10 12:48:31 +0900

    배포 jar로 설정 변경

--- src/main/java/com/simple/blog/BlogApplication.java
+++ src/main/java/com/simple/blog/BlogApplication.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Bean;
 import javax.validation.ConstraintViolationException;
 
 @SpringBootApplication
-public class BlogApplication extends SpringBootServletInitializer {
+public class BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
